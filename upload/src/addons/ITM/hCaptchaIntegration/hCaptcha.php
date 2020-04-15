b42eea97cc10343b8f8183472c77a59d4b8a7064
McAtze <info@it-maku.com>
2020-04-15 19:39:16 +0200

    Update to 1.0.0 Gold

--- upload/src/addons/ITM/hCaptchaIntegration/hCaptcha.php
+++ upload/src/addons/ITM/hCaptchaIntegration/hCaptcha.php
@@ -7,41 +7,53 @@ use XF\Template\Templater;
 
 class hCaptcha extends AbstractCaptcha
 {
-	public function __construct(\XF\App $app)
-	{
+	/**
+	 * @var null|string
+	 */
+	protected $siteKey = null;
+
+	/**
+	 * @var null|string
+	 */
+	protected $privateKey = null;
+
+	protected $verifyUrl = 'https://hcaptcha.com/siteverify';
+
+	public function __construct(\XF\App $app)	{
+		
 		parent::__construct($app);
 		$extraKeys = $app->options()->extraCaptchaKeys;
-		if (!empty($extraKeys['hCaptchaSiteKey']) && !empty($extraKeys['hCaptchaSecretKey']))
-		{
-			$this->siteKey = $extraKeys['hCaptchaSiteKey'];
-			$this->secretKey = $extraKeys['hCaptchaSecretKey'];
-		}
-		if (!empty($extraKeys['hCaptchaWidgetTheme']))
-		{
-			$this->widgetTheme = $extraKeys['hCaptchaWidgetTheme'];
-		}
-		if (!empty($extraKeys['hCaptchaWidgetSize']))
-		{
-			$this->widgetSize = $extraKeys['hCaptchaWidgetSize'];
-		}
+
+		$this->siteKey = $extraKeys['hCaptchaSiteKey'];
+		$this->secretKey = $extraKeys['hCaptchaSecretKey'];
+
+		$this->widgetTheme = $extraKeys['hCaptchaWidgetTheme'];
+		$this->widgetSize = $extraKeys['hCaptchaWidgetSize'];
 	}
-	
-	public function renderInternal(Templater $templater)
-	{
-		if (!$this->siteKey)
-		{
+
+	public function renderInternal(Templater $templater) {
+
+		if (!$this->siteKey || !$this->privateKey) {
 			return '';
 		}
 
+		$ip = $this->app->request()->getIp();
+		$sessionId = md5(uniqid('xfkeycaptcha'));
+		$sign = md5($sessionId . $ip . $this->privateKey);
+		$sign2 = md5($sessionId . $this->privateKey);
+
 		return $templater->renderTemplate('public:itm_captcha_hcaptcha', [
 			'siteKey' => $this->siteKey,
+			'sessionId' => $sessionId,
+			'sign' => $sign,
+			'sign2' => $sign2,
 			'widgetTheme' => $this->widgetTheme,
 			'widgetSize' => $this->widgetSize
 		]);
 	}
 
-	public function isValid()
-	{
+	public function isValid() {
+
 		if (!$this->siteKey || !$this->secretKey)
 		{
 			return true; // if not configured, always pass
@@ -50,32 +62,27 @@ class hCaptcha extends AbstractCaptcha
 		$request = $this->app->request();
 
 		$captchaResponse = $request->filter('h-captcha-response', 'str');
-		if (!$captchaResponse)
-		{
+		if (!$captchaResponse || !is_string($captchaResponse)) {
 			return false;
 		}
 
-		try
-		{
+		$data = [
+				'secret' => $this->privateKey,
+				'response' => $captchaResponse
+		];
+
+		try {
 			$client = $this->app->http()->client();
 
-			$response = $client->post('https://hcaptcha.com/siteverify',
-				['body' => [
-					'secret' => $this->secretKey,
-					'response' => $captchaResponse,
-					'remoteip' => $request->getIp()
-				]
-			])->json();
+			$response = $client->post($this->verifyUrl, [
+					'form_params' => $data
+			]);
 
-			if (isset($response['success']) && isset($response['hostname']) && $response['hostname'] == $request->getHost())
-			{
-				return $response['success'];
-			}
+			$contents = @json_decode($response->getBody()->getContents());
 
-			return false;
+			return (bool)$contents->success;
 		}
-		catch(\GuzzleHttp\Exception\RequestException $e)
-		{
+		catch(\GuzzleHttp\Exception\RequestException $e) {
 			// this is an exception with the underlying request, so let it go through
 			\XF::logException($e, false, 'hCAPTCHA connection error: ');
 			return true;
